5b889200e5d9ac187802e4374469215143e7cbc7
wassname <wassname@users.noreply.github.com>
2020-12-28 20:41:08 +0800

    add resnet feature extraction

--- diy_gym/addons/sensors/camera.py
+++ diy_gym/addons/sensors/camera.py
@@ -3,6 +3,10 @@ import numpy as np
 from gym import spaces
 from diy_gym.model import Model
 from diy_gym.addons.addon import Addon
+import torchvision.models
+from PIL import Image
+import torch
+from torchvision.transforms.functional import to_tensor
 
 
 class Camera(Addon):
@@ -22,13 +26,14 @@ class Camera(Addon):
             expressed in euler angles
         use_depth (bool, optional, False): whether to include depth images in observations
         use_segmentation_mask (bool, optional, False): whether to include segmentation images in observations
+        use_features (bool, optional, False): Use resnet to extract features (must be 256, 256)
     """
     def __init__(self, parent, config):
         super(Camera, self).__init__(parent, config)
 
         self.near, self.far = config.get('clipping_boundaries', [0.01, 100])
         self.fov = config.get('field_of_view', 70.0)
-        self.resolution = config.get('resolution', [640, 480])
+        self.resolution = config.get('resolution', [256, 256])
         self.aspect = self.resolution[0] / self.resolution[1]
 
         self.uid = parent.uid if isinstance(parent, Model) else -1
@@ -39,6 +44,7 @@ class Camera(Addon):
 
         self.use_depth = config.get('use_depth', True)
         self.use_seg_mask = config.get('use_segmentation_mask', False)
+        self.use_features = config.get('use_features', False)
 
         self.T_parent_cam = self.trans_from_xyz_quat(xyz, p.getQuaternionFromEuler(rpy))
         self.projection_matrix = p.computeProjectionMatrixFOV(self.fov, self.aspect, self.near, self.far)
@@ -51,9 +57,37 @@ class Camera(Addon):
         if self.use_depth:
             self.observation_space.spaces.update({'depth': spaces.Box(0., 10., shape=self.resolution, dtype='float32')})
 
+        if self.use_features:
+            resnet18 = torchvision.models.resnet18(pretrained=True).eval()
+            modules=list(resnet18.children())[:-1]
+            self.feature_extractor = torch.nn.Sequential(*modules).eval()
+            self.observation_space.spaces.update(
+                {'features': spaces.Box(-100., 100., shape=(512*2,) if self.use_depth else (512,), dtype='float32')})
+        
         if self.use_seg_mask:
             self.observation_space.spaces.update(
                 {'segmentation_mask': spaces.Box(0., 10., shape=self.resolution, dtype='float32')})
+
+    def extract_features(self, obs):
+        # see https://arxiv.org/pdf/1710.10710.pdf
+
+        normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+        with torch.no_grad():
+            im = Image.fromarray(np.uint8(obs['rgb'] * 255))
+            im = im.resize((224, 224))
+            x1 = normalize(to_tensor(im))
+
+            im = Image.fromarray(np.uint8(obs['depth'] * 255)).convert('RGB')
+            im = im.resize((224, 224))
+            x2 = normalize(to_tensor(im))
+
+            x = torch.stack([x1, x2])
+            h = self.feature_extractor(x)
+            h = h.cpu().detach().numpy() # shape (2, 512)
+        return h.flatten()
+        
 
     def observe(self):
         if self.frame_id >= 0:
@@ -92,6 +126,9 @@ class Camera(Addon):
 
         if self.use_seg_mask:
             obs['segmentation_mask'] = np.array(image[4], copy=False).reshape(self.resolution)
+
+        if self.use_features:
+            obs['features'] = self.extract_features(obs)
 
         return obs
 
